72ef38392b6f800a93d6afe96c8f68bfb4c0d223
Donald <dniskhe@gmail.com>
2024-09-27 01:43:49 +0100

    Code Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authapi',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
